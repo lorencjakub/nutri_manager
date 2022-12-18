6267c152bf5fe1e22ed78ef41247661f10732100
Jakub <jakub.lorencu@gmail.com>
2022-12-18 21:23:08 +0100

    Fix of env vars

--- server/config_prod.py
+++ server/config_prod.py
@@ -4,8 +4,8 @@ from dotenv import load_dotenv
 basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env_prod'))
 
-SECRET_KEY = os.environ.get("SECRET_KEY", default="super-secret-key")
-SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+SECRET_KEY = os.environ.get("SECRET_KEY")
+SQLALCHEMY_DATABASE_URI = os.environ.get("SQLALCHEMY_DATABASE_URI")
 SQLALCHEMY_TRACK_MODIFICATIONS = True if os.environ.get("SQLALCHEMY_TRACK_MODIFICATIONS", default=True) == "True" else False
 SQLALCHEMY_ECHO = True if os.environ.get("SQLALCHEMY_ECHO", default=True) == "True" else False
 FE_ORIGIN = os.environ.get("FE_ORIGIN")
